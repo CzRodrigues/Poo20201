a68240fddb7fb1e41b54bb6e34002a4ce735b13b
StudentCezar <72028625+StudentCezar@users.noreply.github.com>
2020-09-28 12:26:47 -0300

    aluno:Cezar-Work1

---
+++ Projeto 1/Class01.java
@@ -0,0 +1,14 @@
+/* 
+    -Frist Presentation
+    -Student: Cézar Roberto Rocha Rodrigues Filho
+    -Schedule:
+        -Frist PrintIN
+        -Packaga
+        -Application with pachage
+*/
+
+public class Class01 {
+    public static void  main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Projeto 1/src/app/professor/ClassEx3.java
@@ -0,0 +1,10 @@
+package src.app.professor;
+
+public class ClassEx3 {
+    
+    public static void main(String[] args){
+        System.out.println("--------------PROFESSORES-----------");
+        System.out.println("Name: Marcelo Vidigal Gonçalves dos Reis");
+    }
+    
+} 
