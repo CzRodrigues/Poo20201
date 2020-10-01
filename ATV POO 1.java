ff6e3e07c736e91c9324264ad14f5cb8f8de84e8
JoTaR2-java <71936040+JoTaR2-java@users.noreply.github.com>
2020-10-01 15:20:14 -0300

    Add files via upload

---
+++ ATV POO 1.java
@@ -0,0 +1,10 @@
+/* 
+    
+*/
+
+public class ClassEx3
+ {
+    public static void  main(String[] args) {
+        System.out.println("Hello world!"); // Simples exemplo de como dá um primeiro output
+    }
+}
