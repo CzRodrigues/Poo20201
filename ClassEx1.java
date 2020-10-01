831bbcd041f0707bb4463222c329da64a8c85528
JoTaR2-java <71936040+JoTaR2-java@users.noreply.github.com>
2020-10-01 15:33:11 -0300

    Add files via upload

---
+++ ClassEx1.java
@@ -0,0 +1,14 @@
+package src.app.exemplopck
+;
+
+/*
+criação de package e classes e uma saida para o sistema.
+*/
+public class ClassEx1 {
+    
+    public static void main(String[] args){
+        System.out.println("------EXEMPLO------");
+
+    }
+    
+} 
---
+++ ClassEx2.java
@@ -0,0 +1,15 @@
+
+package src.app.nomes;
+
+// Definimos a class usada na pasta em questão com o codigo fonte
+
+public class ClassEx2 {
+    public static void main(String[] args){
+        
+        System.out.println
+        ("Name:João Ricardo da Silva Moreira")
+        ;
+       
+    } 
+       
+}
