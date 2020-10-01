712c770042ffb591cddc1f989d7fc89440f9033b
LuisBelo1 <71555880+LuisBelo1@users.noreply.github.com>
2020-10-01 20:40:09 -0300

    Create OlaMundo.java
    Primeira Aplicação

---
+++ LAB1/OlaMundo.java
@@ -0,0 +1,7 @@
+public class OlaMundo {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Ola, Mundo\n");
+        System.out.println("do ingles\n");
+        System.out.println("Hello World\n");
+    }
+}
