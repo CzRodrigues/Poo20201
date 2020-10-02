cebcb584c83d8d98e7c0dd65e86854d360f5c251
Cezar Rodrigues <72028625+CzRodrigues@users.noreply.github.com>
2020-10-02 16:31:04 -0300

    Redowloading files

---
+++ Projeto 1/src/app/signature/ClassEx2.java
@@ -0,0 +1,36 @@
+package src.app.signature;
+/*
+    Nesta aplicação utilizaremos a pasta encontrada em src => app => signatue 
+    para organizar melhor o nosso projeto em formato de packages, facilitando
+    o entedimento do codigo como um todo
+*/
+
+
+// Definimos a class usada na pasta em questão com o codigo fonte
+
+public class ClassEx2 {
+    public static void main(String[] args){
+        System.out.println(
+            "----------------COWORKES------------------")
+            ;
+        System.out.println
+        ("Name:Cézar Roberto Rocha Rodrigues Filho")
+        ;
+        System.out.println
+        ("Name:João Claudio Oliveira Pinto")
+        ;
+        System.out.println
+        ("Name:João Marcos Vidal Lacerda")
+        ;
+        System.out.println
+        ("Name:João Ricardo da Silva Moreira")
+        ;
+        System.out.println
+        ("Name:Lukas de Menezes")
+        ;
+        System.out.println
+        ("Name:Luís Belo")
+        ;     
+    } 
+        //Definimos acima a saida do nosso executavel para os args.
+}
