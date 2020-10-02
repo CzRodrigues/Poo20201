ff754580521e0e742e05934e68ef271e6c85393a
LuisBelo1 <71555880+LuisBelo1@users.noreply.github.com>
2020-10-01 22:02:18 -0300

    Create Comentarios.java
    Código focado em comentários em Java

---
+++ LAB1/Comentarios.java
@@ -0,0 +1,16 @@
+// Esta eh uma linha de comentarios
+/*
+   Este eh um bloco de comentario
+   linha 1
+   linha 2
+ */
+// Declaracao da classe Comentarios
+public class Comentarios
+{
+     public static void (String[] args) throws Exception
+     {
+        //Comando de printar na tela
+        System.out.println("Isto nao eh um comentario");
+        //Isto eh um comentario
+     }
+}
