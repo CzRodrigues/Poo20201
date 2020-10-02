ad9fd2288148cdfe38b95dec5b44dee963c69a8f
Jomavilla12 <71555896+Jomavilla12@users.noreply.github.com>
2020-10-02 11:32:05 -0300

    Add files via upload

---
+++ Trabalhos01/src/app/nomes/ClassEx2.java
@@ -0,0 +1,36 @@
+package src.app.nomes;
+
+//Criando um package 
+
+/*
+   Aqui utilizamos  a pasta encontrada em src => app => nomes 
+    para organizar melhor o nosso projeto em formato de packages, facilitando
+    o entedimento do codigo como um todo
+*/
+
+public class ClassEx2 {
+    public static void main(String[] args){
+        System.out.println(
+            "========== COLABORADORES ===========")
+            ;
+        System.out.println
+        ("Nome -> Jomavilla")
+        ;
+        System.out.println
+        ("Nome -> João4pintos")
+        ;
+        System.out.println
+        ("Nome -> Jota R2")
+        ;
+        System.out.println
+        ("Nome -> Faneas")
+        ;
+        System.out.println
+        ("Nome -> Casmocaõca")
+        ;
+        System.out.println
+        ("Nome -> Louss - Meu Ad")
+        ;     
+    } 
+        //Definimos acima a saida do nosso executavel para os args.
+}
---
+++ Trabalhos01/src/app/teacher/ClassEx3.java
@@ -0,0 +1,14 @@
+package src.app.teacher;
+
+/*
+Aqui novamente é chamado o package que está dentro do nosso arquivo,
+entretanto, dessa vez chamamos o package professor
+*/
+public class ClassEx3 {
+    
+    public static void main(String[] args){
+        System.out.println("========= PROFESSORES =========");
+        System.out.println("Nome ->  Marcelo Vidigal Gonçalves dos Reis");
+    }
+    
+} 
