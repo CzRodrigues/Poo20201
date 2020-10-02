9d5a6d5f3a067e156aa30fd2ebfd9481e535212a
Jomavilla12 <71555896+Jomavilla12@users.noreply.github.com>
2020-10-02 11:39:25 -0300

    Add files via upload

---
+++ Trabalhos01/ClassEx1.java
@@ -0,0 +1,7 @@
+
+public class ClassEx1 {
+    public static void  main(String[] args) {
+        System.out.println("Olá world! :)"); 
+    }
+}
+// Exemplo de como fazer o output
---
+++ Trabalhos01/src/app/teacher/ClassEx3.java
@@ -0,0 +1,13 @@
+package src.app.teacher;
+
+/*
+Aqui novamente é chamado o nosso package ,entretanto, dessa vez chamamos o package professor
+*/
+public class ClassEx3 {
+    
+    public static void main(String[] args){
+        System.out.println("========= PROFESSORES =========");
+        System.out.println("Nome ->  Marcelo Vidigal");
+    }
+    
+} 
