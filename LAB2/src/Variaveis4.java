2cef961b49cf5531e2b9633917be2643e1990fda
LuisBelo1 <71555880+LuisBelo1@users.noreply.github.com>
2020-10-09 16:43:46 -0300

    Delete Variaveis4.java

--- LAB2/src/Variaveis4.java
+++
@@ -1,8 +0,0 @@
-public class Variaveis4 {
-    public static void main(String[] args) {
-        int primeiroValor = 100;
-        int outroValor = primeiroValor;
-        primeiroValor = 50;
-        System.out.println(String.format("%s, %s", primeiroValor, outroValor));
-    }
-}
