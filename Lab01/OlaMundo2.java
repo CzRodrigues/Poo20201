1dcb09ff5e06944e02f0acc6ee75d28f4817b542
Jomavilla12 <71555896+Jomavilla12@users.noreply.github.com>
2020-10-24 12:28:06 -0300

    Add files via upload

---
+++ Lab01/OlaMundo2.java
@@ -0,0 +1,7 @@
+public class OlaMundo2 {
+	public static void main(String[] args) {
+        System.out.println("olamundo 1!");
+        System.out.println("olamundo 2!");
+        System.out.println("olamundo 3!");
+	}
+}
