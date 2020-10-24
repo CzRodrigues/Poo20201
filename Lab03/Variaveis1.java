279f78181ce90b305e0545c575031c8e1ed35a24
Jomavilla12 <71555896+Jomavilla12@users.noreply.github.com>
2020-10-24 12:30:49 -0300

    Add files via upload

---
+++ Lab03/Variaveis1.java
@@ -0,0 +1,16 @@
+public class Variaveis1{
+
+    public static void main(String[]    args){
+        int valorDado;
+        valorDado = 200;
+
+        int dado = 200;
+
+        int total;
+        int nota4;
+        int saldoContaBancaria;
+        int treinamentoNivel2;
+
+        System.out.println(dado);
+    }
+}    
---
+++ Lab03/Variaveis2.java
@@ -0,0 +1,17 @@
+public class Variaveis2 {
+    
+    public static void main(String[] args){
+        int variavel;
+        System.out.println(variavel);
+        variavel = 50;
+        System.out.println(variavel);
+        int outraVariavel = 100;
+        System.out.println(outraVariavel);
+        variavel = outraVariavel;
+        System.out.printl(variavel);
+        outraVariavel = 200;
+        System.out.println(outraVariavel);
+        System.out.println(variavel);
+
+    }
+}
