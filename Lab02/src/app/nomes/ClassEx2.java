06d4bb7c202d179b1cd53293b8f4892a22a11101
Jomavilla12 <71555896+Jomavilla12@users.noreply.github.com>
2020-10-24 12:30:28 -0300

    Add files via upload

---
+++ Lab02/src/app/nomes/ClassEx2.java
@@ -0,0 +1,36 @@
+package src.app.nomes;
+
+//É criado um package 
+
+/*
+   Aqui utilizamos o package que é encontrado em src -> app -> nomes 
+    para organizar melhor o nosso projeto em formato de packages, para assim facilitar
+    o entedimento do codigo como um todo
+*/
+
+public class ClassEx2 {
+    public static void main(String[] args){
+        System.out.println(
+            "========== VIPS ===========")
+            ;
+        System.out.println
+        ("Nome -> Jomavilla")
+        ;
+        System.out.println
+        ("Nome -> João4pintos")
+        ;
+        System.out.println
+        ("Nome -> Jota R2")
+        ;
+        System.out.println
+        ("Nome -> Faneas")
+        ;
+        System.out.println
+        ("Nome -> Casmocaõca")
+        ;
+        System.out.println
+        ("Nome -> Louss - Meu Ad")
+        ;     
+    } 
+        //Definimos acima a saida do nosso executavel para os args.
+}
