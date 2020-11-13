bccde5a460ee0a8da703e680ff24486daa5090bf
Cezar Student <72028625+CzRodrigues@users.noreply.github.com>
2020-11-13 16:30:22 -0300

    Delete App.java

--- Projeto 2/src/src/Class/App.java
+++
@@ -1,9 +0,0 @@
-package Class;
-
-public class App {
-    
-    public static void main(String[] args) {
-        System.out.println
-        ("This code would show how to use variables into java files, plus how to make mathes operations with them");
-    }
-}
