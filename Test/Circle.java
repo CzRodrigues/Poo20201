a56605c450379d4704c30c06cfbfadf89f3bd45e
Cezar Student <72028625+CzRodrigues@users.noreply.github.com>
2020-11-14 22:56:00 -0300

    Project Bouncing Ball
    Segunda Prova

---
+++ Test/Circle.java
@@ -0,0 +1,12 @@
+public class Circle {
+    double rx, ry;
+    double vx, vy;
+
+    Circle(double rx, double ry, double vx, double vy) {
+        this.rx = rx;
+        this.ry = ry;
+        this.vx = vx;
+        this.vy = vy;
+
+    }
+}
