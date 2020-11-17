202bcbbb534a4bca8ce43dca2937951593760d57
JoTaR2-java <71936040+JoTaR2-java@users.noreply.github.com>
2020-11-17 20:30:49 -0300

    comentando o codigo

--- Test/Circle.java
+++ Test/Circle.java
@@ -1,7 +1,7 @@
 public class Circle {
     double rx, ry;
     double vx, vy;
-
+//criando a funcao de criar os circulos passando como parametros as cordenadas de X e Y e a velocidade em ambos os eixos
     Circle(double rx, double ry, double vx, double vy) {
         this.rx = rx;
         this.ry = ry;
